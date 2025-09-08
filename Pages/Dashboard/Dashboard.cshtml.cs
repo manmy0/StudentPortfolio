3d6e5225fba6beee79c6dc1cfa1a831910d59879
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-09-08 12:54:09 +0930

    Add dashboard page from prototype

---
+++ Pages/Dashboard/Dashboard.cshtml.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentPortfolio.Models;
+using System.Security.Claims;
+
+namespace StudentPortfolio.Pages.Dashboard
+{
+    public class DashboardModel : PageModel
+    {
+        private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DashboardModel(StudentPortfolio.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public ApplicationUser CurrentUser { get; set; }
+        public IList<CompetencyTracker> CompetencyTracker { get; set; } = default!;
+        public IList<Competency> Competencies { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId != null)
+            {
+                CompetencyTracker = await _context.CompetencyTrackers
+                     .Where(i => i.UserId == userId)
+                     .Include(i => i.User)
+                     .ToListAsync();
+
+                var compIds = await _context.CompetencyTrackers
+                                      .Where(i => i.UserId == userId)
+                                      .Select(i => i.CompetencyId)
+                                      .ToListAsync();
+
+                Competencies = await _context.Competencies
+                    .Where(i => compIds.Contains(i.CompetencyId))
+                    .ToListAsync();
+            }
+        }
+    }
+}
+
