870ba6057eb500359533dd7da242676e64297f51
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-09-09 14:57:03 +0930

    Ensure that model is validated before form can be submitted

--- Pages/Goals/Create.cshtml.cs
+++ Pages/Goals/Create.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -6,12 +7,14 @@ using StudentPortfolio.Data;
 using StudentPortfolio.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace StudentPortfolio.Pages.Goals
 {
+    [Authorize]
     public class CreateModel : PageModel
     {
         private readonly StudentPortfolio.Data.ApplicationDbContext _context;
@@ -25,21 +28,21 @@ namespace StudentPortfolio.Pages.Goals
 
         public Goal Goal { get; set; }
 
-        public async Task<IActionResult> OnPostAsync([Bind("Description, Timeline, Startdate, Enddate")] Goal Goal)
+        public async Task<IActionResult> OnPostAsync([Bind("Description, Timeline, StartDate, EndDate")] Goal Goal)
         {
+            Goal.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (!ModelState.IsValid)   
             {
-                string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                return Page();
+                
+            }
 
-                if (userId != null)
-                {
-                    Goal.UserId = userId;
+            _context.Goals.Add(Goal);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("Goals");
 
-                }
-
-                _context.Goals.Add(Goal);
-                await _context.SaveChangesAsync();
-                return RedirectToPage("Goals");
             }
         }
     }
-}
+
