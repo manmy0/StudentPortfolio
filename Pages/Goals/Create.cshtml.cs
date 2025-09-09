3ad8ae18920408d0e3e76870468a76077afee76d
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-09-09 10:52:13 +0930

    Scaffold create page for goals

---
+++ Pages/Goals/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StudentPortfolio.Data;
+using StudentPortfolio.Models;
+
+namespace StudentPortfolio.Pages.Goals
+{
+    public class CreateModel : PageModel
+    {
+        private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+
+        public CreateModel(StudentPortfolio.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public Goal Goal { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Goals.Add(Goal);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
