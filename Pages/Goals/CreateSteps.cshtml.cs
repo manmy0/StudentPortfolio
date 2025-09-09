aa0d88e7c0485795757e727a8ca53184f56ab99d
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-09-09 15:44:36 +0930

    Scaffold create page for steps and add button for each goal in the table that does not have any associated steps

---
+++ Pages/Goals/CreateSteps.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StudentPortfolio.Data;
+using StudentPortfolio.Models;
+
+namespace StudentPortfolio.Pages.Goals
+{
+    public class CreateStepsModel : PageModel
+    {
+        private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+
+        public CreateStepsModel(StudentPortfolio.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["GoalId"] = new SelectList(_context.Goals, "GoalId", "Description");
+            return Page();
+        }
+
+        [BindProperty]
+        public GoalStep GoalStep { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.GoalSteps.Add(GoalStep);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
