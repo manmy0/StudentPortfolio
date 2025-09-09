9739362f5224286c89554cbf16f81cf1b419c62e
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-09-09 13:16:39 +0930

    Form creates new record in database successfully

--- Pages/Goals/Create.cshtml.cs
+++ Pages/Goals/Create.cshtml.cs
@@ -1,45 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using StudentPortfolio.Data;
 using StudentPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace StudentPortfolio.Pages.Goals
 {
     public class CreateModel : PageModel
     {
         private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public CreateModel(StudentPortfolio.Data.ApplicationDbContext context)
+        public CreateModel(StudentPortfolio.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult OnGet()
-        {
-        ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
-            return Page();
-        }
+        public Goal Goal { get; set; }
 
-        [BindProperty]
-        public Goal Goal { get; set; } = default!;
-
-        // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync([Bind("Description, Timeline, Startdate, Enddate")] Goal Goal)
         {
-            if (!ModelState.IsValid)
             {
-                return Page();
+                string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                if (userId != null)
+                {
+                    Goal.UserId = userId;
+
+                }
+
+                _context.Goals.Add(Goal);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("Goals");
             }
-
-            _context.Goals.Add(Goal);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
         }
     }
 }
