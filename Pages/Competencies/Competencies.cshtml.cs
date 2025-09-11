c085d804786d52a63bb8365159cdd17664c71993
Otslc <otslc001@mymail.unisa.edu.au>
2025-09-12 02:17:35 +0930

    Cleaned up competencies dropdown development

--- Pages/Competencies/Competencies.cshtml.cs
+++ Pages/Competencies/Competencies.cshtml.cs
@@ -61,20 +61,5 @@ namespace StudentPortfolio.Pages.Competencies
                     .ToListAsync();
             }
         }
-
-        /*public void OnGet()
-        {
-
-            MyDropdownItems = new List<SelectListItem>
-        {
-            new SelectListItem { Text = "Option A", Value = "A", },
-            new SelectListItem { Text = "Option B", Value = "B", Selected = true }, // Pre-select an item
-            new SelectListItem { Text = "Option C", Value = "C" }
-        };
-            MyDropdown = new List<CompetencyTracker>
-            {
-
-            }
-        }*/
     }
 }
