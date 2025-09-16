78fbe84a6fe10393835f110f7f85bbb8c708106e
BenDavies2 <benjamin@the-davies.net>
2025-09-16 13:49:17 +0930

    Counting number of trackers for a competency
    Updated the controller to return to the razor page how many trackers there are for a competency. This will show how many reviews have been done for a competency.

--- Pages/Dashboard/Dashboard.cshtml.cs
+++ Pages/Dashboard/Dashboard.cshtml.cs
@@ -24,6 +24,9 @@ namespace StudentPortfolio.Pages.Dashboard
         public IList<CompetencyTracker> CompetencyTracker { get; set; } = default!;
         public IList<Competency> Competencies { get; set; } = default!;
 
+        // dictionary to store the count of how many entries there are for a specific competency
+        public IDictionary<long, int> CompetencyCounts { get; set; } = new Dictionary<long, int>();
+
         public async Task OnGetAsync()
         {
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -33,16 +36,26 @@ namespace StudentPortfolio.Pages.Dashboard
                 CompetencyTracker = await _context.CompetencyTrackers
                      .Where(i => i.UserId == userId)
                      .Include(i => i.User)
+                     .Include(i => i.Competency)
                      .ToListAsync();
 
+                // find the competencyIds that the user has entries for
                 var compIds = await _context.CompetencyTrackers
                                       .Where(i => i.UserId == userId)
                                       .Select(i => i.CompetencyId)
                                       .ToListAsync();
 
+                // get all competencies that the user has a competency tracker for
                 Competencies = await _context.Competencies
                     .Where(i => compIds.Contains(i.CompetencyId))
                     .ToListAsync();
+
+                // populate the dictionary that has the id of the competency as the key
+                // and the number of times it occurs as the value
+                CompetencyCounts = await _context.CompetencyTrackers
+                    .Where(i => i.UserId == userId)
+                    .GroupBy(i => i.CompetencyId)
+                    .ToDictionaryAsync(g => g.Key, g => g.Count());
             }
         }
     }
