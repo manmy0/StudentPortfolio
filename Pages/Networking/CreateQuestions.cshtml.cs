9878bb3dec1f267544294417fc1adb07deaee684
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-09-22 15:06:38 +0930

    New questions can be added for a event

--- Pages/Networking/CreateQuestions.cshtml.cs
+++ Pages/Networking/CreateQuestions.cshtml.cs
@@ -1,45 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using StudentPortfolio.Data;
 using StudentPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace StudentPortfolio.Pages.Networking
 {
     public class CreateQuestionsModel : PageModel
     {
         private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public CreateQuestionsModel(StudentPortfolio.Data.ApplicationDbContext context)
+
+        public CreateQuestionsModel(StudentPortfolio.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
+
         }
 
-        public IActionResult OnGet()
-        {
-        ViewData["EventId"] = new SelectList(_context.NetworkingEvents, "EventId", "EventId");
-            return Page();
-        }
+        [BindProperty(Name = "eventId", SupportsGet = true)]
+        public int Id { get; set; }
+
 
         [BindProperty]
-        public NetworkingQuestion NetworkingQuestion { get; set; } = default!;
+        public List<NetworkingQuestion> NetworkingQuestions { get; set; } = new List<NetworkingQuestion>();
 
-        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public IList<NetworkingEvent> NetworkingEvent { get; set; } = default!;
+
+        public async Task OnGet()
+        {
+            NetworkingQuestions.Add(new NetworkingQuestion());
+
+            NetworkingEvent = await _context.NetworkingEvents
+                .Where(g => g.EventId == Id)
+                .ToListAsync();
+
+        }
+
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            var idExists = await _context.NetworkingEvents.AnyAsync(g => g.EventId == Id);
+          
+            if (!idExists)
             {
+                ModelState.AddModelError("NetworkingEvents.EventId", "Selected Event does not exist.");
+
                 return Page();
             }
 
-            _context.NetworkingQuestions.Add(NetworkingQuestion);
+            if (NetworkingQuestions.Count > 0)
+            {
+                foreach (var question in NetworkingQuestions)
+                {
+                    question.EventId = Id;
+                }
+            }
+
+
+            _context.NetworkingQuestions.AddRange(NetworkingQuestions);
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("Networking");
         }
     }
 }
