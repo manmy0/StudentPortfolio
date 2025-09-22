b3e597e9be6f14fd42e76a7bc11718bf6d0103e7
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-09-22 10:16:26 +0930

    Scaffold edit page for upcoming events

---
+++ Pages/Networking/EditEvent.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudentPortfolio.Data;
+using StudentPortfolio.Models;
+
+namespace StudentPortfolio.Pages.Networking
+{
+    public class EditEventModel : PageModel
+    {
+        private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+
+        public EditEventModel(StudentPortfolio.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public NetworkingEvent NetworkingEvent { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var networkingevent =  await _context.NetworkingEvents.FirstOrDefaultAsync(m => m.EventId == id);
+            if (networkingevent == null)
+            {
+                return NotFound();
+            }
+            NetworkingEvent = networkingevent;
+           ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(NetworkingEvent).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NetworkingEventExists(NetworkingEvent.EventId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool NetworkingEventExists(long id)
+        {
+            return _context.NetworkingEvents.Any(e => e.EventId == id);
+        }
+    }
+}
