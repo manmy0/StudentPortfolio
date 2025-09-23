d4e6328471799c94e73ac507c978427b6230bf01
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-09-23 12:29:25 +0930

    A new contact can be successfully added

--- Pages/Networking/CreateContact.cshtml.cs
+++ Pages/Networking/CreateContact.cshtml.cs
@@ -1,45 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using StudentPortfolio.Data;
 using StudentPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace StudentPortfolio.Pages.Networking
 {
     public class CreateContactModel : PageModel
     {
         private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public CreateContactModel(StudentPortfolio.Data.ApplicationDbContext context)
+
+        public CreateContactModel(StudentPortfolio.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
-        }
-
-        public IActionResult OnGet()
-        {
-        ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
-            return Page();
+            _userManager = userManager;
         }
 
         [BindProperty]
         public IndustryContactLog IndustryContactLog { get; set; } = default!;
 
-        // For more information, see https://aka.ms/RazorPagesCRUD.
+        [BindProperty]
+        public string? PhoneNumber { get; set; }
+
+        [BindProperty]
+        public string? EmailAddress { get; set; }
+
+        [BindProperty]
+        public string? LinkedInUrl { get; set; }
+
+        [BindProperty]
+        public string? OtherContactType { get; set; }
+
+        [BindProperty]
+        public string? OtherContactDetails { get; set; }
+
         public async Task<IActionResult> OnPostAsync()
         {
+            IndustryContactLog.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
+            IndustryContactLog.IndustryContactInfos = new List<IndustryContactInfo>();
+
+            if (!string.IsNullOrWhiteSpace(PhoneNumber))
+            {
+                IndustryContactLog.IndustryContactInfos.Add(new IndustryContactInfo
+                {
+                    ContactType = "Phone",
+                    ContactDetails = PhoneNumber
+                });
+            }
+
+            if (!string.IsNullOrWhiteSpace(EmailAddress))
+            {
+                IndustryContactLog.IndustryContactInfos.Add(new IndustryContactInfo
+                {
+                    ContactType = "Email",
+                    ContactDetails = EmailAddress
+                });
+            }
+
+            if (!string.IsNullOrWhiteSpace(LinkedInUrl))
+            {
+                IndustryContactLog.IndustryContactInfos.Add(new IndustryContactInfo
+                {
+                    ContactType = "LinkedIn",
+                    ContactDetails = LinkedInUrl
+                });
+            }
+
+            if (!string.IsNullOrWhiteSpace(OtherContactDetails))
+            {
+                IndustryContactLog.IndustryContactInfos.Add(new IndustryContactInfo
+                {
+                    ContactType = string.IsNullOrWhiteSpace(OtherContactType) ? "Other" : OtherContactType,
+                    ContactDetails = OtherContactDetails
+                });
+            }
+
             _context.IndustryContactLogs.Add(IndustryContactLog);
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("Networking");
         }
     }
 }
