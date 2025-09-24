5855661f93e2d97ab5f594734207ef3f79b99801
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-09-24 15:47:54 +0930

    Most fields can be updated with the edit page

--- Pages/Networking/EditContact.cshtml.cs
+++ Pages/Networking/EditContact.cshtml.cs
@@ -1,27 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using StudentPortfolio.Data;
 using StudentPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace StudentPortfolio.Pages.Networking
 {
     public class EditContactModel : PageModel
     {
         private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public EditContactModel(StudentPortfolio.Data.ApplicationDbContext context)
+        private const string PhoneType = "Phone";
+        private const string EmailType = "Email";
+        private const string LinkedInType = "LinkedIn";
+
+        public EditContactModel(StudentPortfolio.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [BindProperty]
         public IndustryContactLog IndustryContactLog { get; set; } = default!;
+
+        public List<IndustryContactInfo> IndustryContactInfo { get; set; }  = new List<IndustryContactInfo>();
+
+        [BindProperty]
+        public string? PhoneNumber { get; set; }
+
+        [BindProperty]
+        public string? EmailAddress { get; set; }
+
+        [BindProperty]
+        public string? LinkedInUrl { get; set; }
+
+        [BindProperty]
+        public string? OtherContactType { get; set; }
+
+        [BindProperty]
+        public string? OtherContactDetails { get; set; }
 
         public async Task<IActionResult> OnGetAsync(long? id)
         {
@@ -30,26 +55,72 @@ namespace StudentPortfolio.Pages.Networking
                 return NotFound();
             }
 
-            var industrycontactlog =  await _context.IndustryContactLogs.FirstOrDefaultAsync(m => m.ContactId == id);
+           var industrycontactlog = await _context.IndustryContactLogs
+                .Include(log => log.IndustryContactInfos)
+                .FirstOrDefaultAsync(m => m.ContactId == id);
+
             if (industrycontactlog == null)
             {
                 return NotFound();
             }
+
             IndustryContactLog = industrycontactlog;
-           ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+
+            PhoneNumber = industrycontactlog.IndustryContactInfos
+                .FirstOrDefault(info => info.ContactType == PhoneType)?.ContactDetails;
+
+            EmailAddress = industrycontactlog.IndustryContactInfos
+                .FirstOrDefault(info => info.ContactType == EmailType)?.ContactDetails;
+
+            LinkedInUrl = industrycontactlog.IndustryContactInfos
+                .FirstOrDefault(info => info.ContactType == LinkedInType)?.ContactDetails;
+
+            var otherInfo = industrycontactlog.IndustryContactInfos
+                .FirstOrDefault(info => info.ContactType != PhoneType && info.ContactType != EmailType && info.ContactType != LinkedInType);
+
+            if (otherInfo != null)
+            {
+                OtherContactType = otherInfo.ContactType;
+                OtherContactDetails = otherInfo.ContactDetails;
+            }
+
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
+            IndustryContactLog.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
             _context.Attach(IndustryContactLog).State = EntityState.Modified;
+
+            await UpdateContactInfoAsync(IndustryContactLog.ContactId, PhoneType, PhoneNumber);
+            await UpdateContactInfoAsync(IndustryContactLog.ContactId, EmailType, EmailAddress);
+            await UpdateContactInfoAsync(IndustryContactLog.ContactId, LinkedInType, LinkedInUrl);
+
+            var existingOther = await _context.IndustryContactInfos
+                .FirstOrDefaultAsync(i => i.ContactId == IndustryContactLog.ContactId &&
+                                           i.ContactType != PhoneType &&
+                                           i.ContactType != EmailType &&
+                                           i.ContactType != LinkedInType);
+            if (existingOther != null)
+            {
+                _context.IndustryContactInfos.Remove(existingOther);
+            }
+          
+            if (!string.IsNullOrWhiteSpace(OtherContactType) && !string.IsNullOrWhiteSpace(OtherContactDetails))
+            {
+                _context.IndustryContactInfos.Add(new IndustryContactInfo
+                {
+                    ContactId = IndustryContactLog.ContactId,
+                    ContactType = OtherContactType,
+                    ContactDetails = OtherContactDetails
+                });
+            }
 
             try
             {
@@ -67,7 +138,34 @@ namespace StudentPortfolio.Pages.Networking
                 }
             }
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("Networking");
+        }
+
+        private async Task UpdateContactInfoAsync(long contactId, string contactType, string? newDetails)
+        {
+            var existingInfo = await _context.IndustryContactInfos
+                .FirstOrDefaultAsync(i => i.ContactId == contactId && i.ContactType == contactType);
+
+            if (!string.IsNullOrWhiteSpace(newDetails))
+            {
+                if (existingInfo != null)
+                {
+                    existingInfo.ContactDetails = newDetails;
+                }
+                else
+                {
+                    _context.IndustryContactInfos.Add(new IndustryContactInfo
+                    {
+                        ContactId = contactId,
+                        ContactType = contactType,
+                        ContactDetails = newDetails
+                    });
+                }
+            }
+            else if (existingInfo != null)
+            {
+                _context.IndustryContactInfos.Remove(existingInfo);
+            }
         }
 
         private bool IndustryContactLogExists(long id)
@@ -75,4 +173,5 @@ namespace StudentPortfolio.Pages.Networking
             return _context.IndustryContactLogs.Any(e => e.ContactId == id);
         }
     }
+
 }
