89ba93b92767aba58b95c7256c8ee2f691863017
Otslc <otslc001@mymail.unisa.edu.au>
2025-09-27 16:54:01 +0930

    Added Level class, competencyTrackerId, competencyDisplayId, and levelId

---
+++ Migrations/20250927054718_updateCompetency.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentPortfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateCompetency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CompetencyTracker",
+                table: "CompetencyTracker");
+
+            migrationBuilder.DropColumn(
+                name: "level",
+                table: "CompetencyTracker");
+
+            migrationBuilder.AddColumn<long>(
+                name: "competencyTrackerId",
+                table: "CompetencyTracker",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<long>(
+                name: "levelId",
+                table: "CompetencyTracker",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 1L);
+
+            migrationBuilder.AddColumn<string>(
+                name: "competencyDisplayId",
+                table: "Competency",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CompetencyTracker",
+                table: "CompetencyTracker",
+                column: "competencyTrackerId");
+
+            migrationBuilder.CreateTable(
+                name: "Level",
+                columns: table => new
+                {
+                    levelId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    rank = table.Column<short>(type: "smallint", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Level", x => x.levelId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompetencyTracker_levelId",
+                table: "CompetencyTracker",
+                column: "levelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompetencyTracker_userId",
+                table: "CompetencyTracker",
+                column: "userId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_CompetencyTracker_Id",
+                table: "CompetencyTracker",
+                column: "competencyTrackerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_Competency_Id",
+                table: "Competency",
+                column: "competencyId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_Level_levelId",
+                table: "Level",
+                column: "levelId",
+                unique: true);
+
+            //migrationBuilder.AddForeignKey(
+            //    name: "FK_CompetencyTracker_Level",
+            //    table: "CompetencyTracker",
+            //    column: "levelId",
+            //    principalTable: "Level",
+            //    principalColumn: "levelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CompetencyTracker_Level",
+                table: "CompetencyTracker");
+
+            migrationBuilder.DropTable(
+                name: "Level");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CompetencyTracker",
+                table: "CompetencyTracker");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CompetencyTracker_levelId",
+                table: "CompetencyTracker");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CompetencyTracker_userId",
+                table: "CompetencyTracker");
+
+            migrationBuilder.DropIndex(
+                name: "UQ_CompetencyTracker_Id",
+                table: "CompetencyTracker");
+
+            migrationBuilder.DropIndex(
+                name: "UQ_Competency_Id",
+                table: "Competency");
+
+            migrationBuilder.DropColumn(
+                name: "competencyTrackerId",
+                table: "CompetencyTracker");
+
+            migrationBuilder.DropColumn(
+                name: "levelId",
+                table: "CompetencyTracker");
+
+            migrationBuilder.DropColumn(
+                name: "competencyDisplayId",
+                table: "Competency");
+
+            migrationBuilder.AddColumn<string>(
+                name: "level",
+                table: "CompetencyTracker",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "Emerging");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CompetencyTracker",
+                table: "CompetencyTracker",
+                columns: new[] { "userId", "competencyId", "level" });
+        }
+    }
+}
--- Models/CompetencyTracker.cs
+++ Models/CompetencyTracker.cs
@@ -6,6 +6,8 @@ namespace StudentPortfolio.Models;
 
 public partial class CompetencyTracker
 {
+    public long CompetencyTrackerId { get; set; }
+
     public string UserId { get; set; } = null!;
 
     public long CompetencyId { get; set; }
@@ -14,7 +16,7 @@ public partial class CompetencyTracker
 
     public DateOnly EndDate { get; set; }
 
-    public string Level { get; set; } = null!;
+    public long LevelId { get; set; }
 
     public string? SkillsReview { get; set; }
 
@@ -27,4 +29,6 @@ public partial class CompetencyTracker
     public virtual Competency Competency { get; set; } = null!;
 
     public virtual ApplicationUser User { get; set; } = null!;
+
+    public virtual Level Level { get; set; } = null!;
 }
