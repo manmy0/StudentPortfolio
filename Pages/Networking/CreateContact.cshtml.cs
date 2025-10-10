1fc7d21043144dbe2c1bffd7adfa038370b7cb07
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-10-10 16:51:54 +1030

    Add buttons to all tables and adjust css in edit pages

--- Pages/Networking/CreateContact.cshtml.cs
+++ Pages/Networking/CreateContact.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -12,6 +13,7 @@ using System.Threading.Tasks;
 
 namespace StudentPortfolio.Pages.Networking
 {
+    [Authorize]
     public class CreateContactModel : PageModel
     {
         private readonly StudentPortfolio.Data.ApplicationDbContext _context;
