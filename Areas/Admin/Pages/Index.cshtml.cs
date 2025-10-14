51a6e12edd4d603a32aa20510a9be2eb7b33bc22
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-10-14 13:02:39 +1030

    Add roles by creating a model that seeds them, creates a base admin account and then redirects based on what role an account has when logging in

---
+++ Areas/Admin/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentPortfolio.Areas.Admin.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
