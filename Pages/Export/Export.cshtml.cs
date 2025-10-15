d0ef4885f7aefcd2226f7704028e1f78bdfc105c
BenDavies2 <benjamin@the-davies.net>
2025-10-16 10:02:06 +1030

    Export Updates
    Can now use the check boxes to select which parts to export. Still need to get the trickier sections of the export working

--- Pages/Export/Export.cshtml.cs
+++ Pages/Export/Export.cshtml.cs
@@ -16,6 +16,24 @@ namespace StudentPortfolio.Pages.Export
 
         public ApplicationUser CurrentUser { get; set; }
 
+        [BindProperty]
+        public bool ExportSummary {  get; set; }
+
+        [BindProperty]
+        public bool ExportPitch { get; set; }
+
+        [BindProperty]
+        public bool ExportCompetencies { get; set; }
+
+        [BindProperty]
+        public bool ExportResume { get; set; }
+
+        [BindProperty]
+        public bool ExportCoverLetter { get; set; }
+
+        [BindProperty]
+        public bool ExportGoals { get; set; }
+
         public ExportModel(StudentPortfolio.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
@@ -33,7 +51,7 @@ namespace StudentPortfolio.Pages.Export
 
         }
 
-        public async Task<IActionResult> OnPostExportSummaryAsCSV()
+        public async Task<IActionResult> OnPostExportPortfolio()
         {
             // have to grab the user again to get the right information
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -45,21 +63,39 @@ namespace StudentPortfolio.Pages.Export
             // if not null then we can grab the info we need
             CurrentUser = await _userManager.FindByIdAsync(userId);
 
-            var summary = CurrentUser.Introduction;
-
-            // heading of personal summary, underneat it append their summary
-            // or that there is no summary provided
             var sb = new StringBuilder();
-            sb.AppendLine("Personal Summary");
 
-            sb.AppendLine(summary ?? "No summary provided.");
+            if (ExportSummary)
+            {
+                var summary = CurrentUser.Introduction;
+
+                sb.AppendLine("Personal Summary");
+                sb.AppendLine(summary ?? "No personal summary found.");
+                sb.AppendLine();
+            }
+
+            if (ExportPitch)
+            {
+                var pitch = CurrentUser.Pitch;
+
+                sb.AppendLine("Elevator Pitch");
+                sb.AppendLine(pitch ?? "No elevator pitch found.");
+                sb.AppendLine();
+            }
+
+            // just refresh the page if nothing was exported
+            if (sb.Length == 0)
+            {
+                return RedirectToPage();
+            }
+            
 
             // encode it to a byte array so it can be exported
             var bytes = Encoding.UTF8.GetPreamble()
                 .Concat(Encoding.UTF8.GetBytes(sb.ToString()))
                 .ToArray();
 
-            return File(bytes, "text/csv", "Personal_Summary.csv");
+            return File(bytes, "text/csv", "Portfolio_Export.csv");
         }
 
         
