25532a82c47d0ae42c906384d38d6a4356d4c298
BenDavies2 <benjamin@the-davies.net>
2025-10-23 09:30:54 +1030

    Staff Export Functionality
    Now able to export just the personal summary of any of the students. I am reusing code which is a shame but it will be a lot of work to structure it so methods can be used across both staff and student pages

--- Areas/Staff/Pages/Export.cshtml.cs
+++ Areas/Staff/Pages/Export.cshtml.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using StudentPortfolio.Models;
+using System.IO.Compression;
+using System.Security.Claims;
+using System.Text;
 
 namespace StudentPortfolio.Areas.Staff.Pages
 {
@@ -28,15 +31,100 @@ namespace StudentPortfolio.Areas.Staff.Pages
 
         public async Task<IActionResult> OnPostExportIndividualStudentDataAsync(string studentId)
         {
-            Users = await _userManager.GetUsersInRoleAsync("Student");
+            var user = await _context.Users.FindAsync(studentId);
 
-            return Page();
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var exportData = new Dictionary<string, StringBuilder>();
+
+            // get the date and user name for the file name
+            var userName = user.FirstName + "_" + user.LastName;
+            var date = DateTime.Now;
+            var dateString = date.ToString("dd-MM-yyyy");
+
+            var fileName = $"{userName}_Personal_Summary_{dateString}.csv";
+            exportData.Add(fileName, GetSummaryCsv(user));
+
+            return CreateZipFile(exportData, user);
         }
 
         public async Task<IActionResult> OnPostExportAllStudentDataAsync()
         {
             Users = await _userManager.GetUsersInRoleAsync("Student");
             return Page();
+        }
+
+        public static string CleanCSV(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return "";
+
+            // double any quote
+            value = value.Replace("\"", "\"\"");
+
+            // wrap a comma, quote or new line in quotes
+            if (value.Contains(',') || value.Contains('"') || value.Contains('\n') || value.Contains('\r'))
+            {
+                value = $"\"{value}\"";
+            }
+
+            return value;
+        }
+
+        private StringBuilder GetSummaryCsv(ApplicationUser user)
+        {
+            var sb = new StringBuilder();
+            var summary = user.Introduction ?? "No personal summary found.";
+
+            sb.AppendLine("Personal Summary");
+            sb.AppendLine(CleanCSV(summary));
+            sb.AppendLine();
+            return sb;
+        }
+
+        private IActionResult CreateZipFile(Dictionary<string, StringBuilder> exportData, ApplicationUser user)
+        {
+            // create a new memory stream object to temporarily hold the zip file while its being built
+            using (var memoryStream = new MemoryStream())
+            {
+
+                // create a zip archive object to build the zip
+                using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                {
+
+                    // loop through all the files that need to be created from dict
+                    foreach (var kvp in exportData)
+                    {
+                        string fileName = kvp.Key;
+                        StringBuilder content = kvp.Value;
+
+                        if (content.Length > 0)
+                        {
+
+                            // create new file
+                            var entry = archive.CreateEntry(fileName);
+                            using (var entryStream = entry.Open())
+                            using (var writer = new StreamWriter(entryStream, Encoding.UTF8))
+                            {
+                                // write to the file
+                                writer.Write(content.ToString());
+                            }
+                        }
+                    }
+                }
+
+                // create the file name
+                var userName = user.FirstName + "_" + user.LastName;
+                var date = DateTime.Now;
+                var dateString = date.ToString("dd-MM-yyyy");
+                var finalFileName = $"{userName}_Portfolio_Export_{dateString}.zip";
+
+                // export the zip file
+                return File(memoryStream.ToArray(), "application/zip", finalFileName);
+            }
         }
     }
 }
