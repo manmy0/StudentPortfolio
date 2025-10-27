ea9cb141d0a27fc2b45668e7f363b1afb45c04b5
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-10-27 15:15:47 +1030

    Add page for viewing student data

---
+++ Areas/Staff/Pages/ViewStudentData.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentPortfolio.Areas.Staff.Pages
+{
+    public class ViewStudentDataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
