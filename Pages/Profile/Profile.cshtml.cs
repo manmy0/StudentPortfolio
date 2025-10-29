e5b8f23798fcc7b077070970e0ce7d2280116042
Otslc <otslc001@mymail.unisa.edu.au>
2025-10-29 12:39:58 +1030

    Changed name on user Profile page

--- Pages/Profile/Profile.cshtml.cs
+++ Pages/Profile/Profile.cshtml.cs
@@ -30,6 +30,8 @@ namespace StudentPortfolio.Pages.Profile
         public CareerDevelopmentPlan CDP { get; set; }
         public IList<UserLink> UserLinks { get; set; } = default!;
 
+        public String UsersName { get; set; } = "";
+
         // convert the users byte representation of an image
         // in the database to base64 which can be displayed
         public string? ProfileImageBase64 =>
@@ -53,7 +55,24 @@ namespace StudentPortfolio.Pages.Profile
                 UserLinks = await _context.UserLinks
                     .Where(l => l.UserId == userId)
                     .ToListAsync();
-                   
+
+                if (!string.IsNullOrWhiteSpace(CurrentUser.PreferedFirstName))
+                {
+                    UsersName = CurrentUser.PreferedFirstName;
+                    if (!string.IsNullOrWhiteSpace(CurrentUser.LastName))
+                        UsersName += $" {CurrentUser.LastName}";
+                }
+                else if (!string.IsNullOrWhiteSpace(CurrentUser.FirstName))
+                {
+                    UsersName = CurrentUser.FirstName;
+                    if (!string.IsNullOrWhiteSpace(CurrentUser.LastName))
+                        UsersName += $" {CurrentUser.LastName}";
+                }
+                else if (!string.IsNullOrWhiteSpace(CurrentUser.LastName))
+                {
+                    UsersName = CurrentUser.LastName;
+                }
+
             }
 
         }
