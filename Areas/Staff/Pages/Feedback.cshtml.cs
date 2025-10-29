1630d2e8646c615513d4c1ceba213e8ac2b6aab4
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-10-29 11:25:19 +1030

    Starting feedback page

--- Areas/Staff/Pages/Feedback.cshtml.cs
+++ Areas/Staff/Pages/Feedback.cshtml.cs
@@ -1,12 +1,29 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentPortfolio.Models;
 
 namespace StudentPortfolio.Areas.Staff.Pages
 {
     public class FeedbackModel : PageModel
     {
-        public void OnGet()
+        private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public FeedbackModel(StudentPortfolio.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
+
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public int? goalId{ get; set; }
+
+        public async Task OnGetAsync()
+        {
+
+
         }
     }
 }
