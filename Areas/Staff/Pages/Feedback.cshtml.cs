c5c4b0e5c06e81a722a38ea9169b95edda1775d1
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-10-29 15:21:56 +1030

    Feedback can be given for a goal

--- Areas/Staff/Pages/Feedback.cshtml.cs
+++ Areas/Staff/Pages/Feedback.cshtml.cs
@@ -1,12 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using StudentPortfolio.Models;
 using StudentPortfolio.Pages.Goals;
+using System.Security.Claims;
 
 namespace StudentPortfolio.Areas.Staff.Pages
 {
+    [Authorize(Roles="Staff")]
     public class FeedbackModel : PageModel
     {
         private readonly StudentPortfolio.Data.ApplicationDbContext _context;
@@ -22,28 +25,55 @@ namespace StudentPortfolio.Areas.Staff.Pages
         [BindProperty(SupportsGet = true)]
         public int? goalId { get; set; }
 
-        public string Feedback {  get; set; }
+        public ApplicationUser CurrentUser { get; set; }
 
         public Goal SelectedGoal { get; set; }
 
+        [BindProperty]
+        public Feedback Feedback { get; set; } = new Feedback();
+
         public IList<GoalStep> GoalSteps { get; set; }
 
-        public async Task OnGetAsync()
+        private async Task LoadGoalDataAsync()
         {
+
             SelectedGoal = await _context.Goals.SingleOrDefaultAsync(i => i.GoalId == goalId);
 
             GoalSteps = await _context.GoalSteps
-                        .Where(i => i.GoalId == goalId)
-                        .ToListAsync();
+                                .Where(i => i.GoalId == goalId)
+                                .ToListAsync();
+        }
+
+        public async Task OnGetAsync()
+        {
+            await LoadGoalDataAsync();
 
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            return Page();
+            if (userId == null)
+            {
+                return NotFound();
+            }
 
-          
+            CurrentUser = await _userManager.FindByIdAsync(userId);
+
+            Feedback.UserId = CurrentUser.Id;
+            Feedback.GoalId = goalId;
+
+            if (!ModelState.IsValid)
+            {
+                await LoadGoalDataAsync();
+                return Page();
+
+            }
+
+            _context.Feedbacks.Add(Feedback);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("/Students");
 
         }
     }
--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -1,13 +1,18 @@
-﻿namespace StudentPortfolio.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace StudentPortfolio.Models
 {
     public class Feedback
     {
         public int FeedbackId { get; set; }
-        
+
+        [ValidateNever]
         public string UserId { get; set; }
 
+        [ValidateNever]
         public long? GoalId { get; set; }
 
+        [ValidateNever]
         public long? CompetencyTrackerId { get; set; }
 
         public string FeedbackText { get; set; }
@@ -16,10 +21,13 @@
 
         public DateTime DateUpdated { get; set; }
 
+        [ValidateNever]
         public virtual ApplicationUser User { get; set; } = null!;
 
+        [ValidateNever]
         public virtual Goal Goal { get; set; } = null!;
 
+        [ValidateNever]
         public virtual CompetencyTracker CompetencyTracker { get; set; } = null!;
 
 
