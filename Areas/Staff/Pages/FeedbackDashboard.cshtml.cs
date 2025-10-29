9e512b7fcc7f4313176abec700f5ad34e3a88859
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-10-29 16:25:16 +1030

    Create dashboard for feedback

---
+++ Areas/Staff/Pages/FeedbackDashboard.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentPortfolio.Data;
+using StudentPortfolio.Models;
+
+namespace StudentPortfolio.Areas.Staff.Pages
+{
+    public class FeedbackDashboard2Model : PageModel
+    {
+        private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+
+        public FeedbackDashboard2Model(StudentPortfolio.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Feedback> Feedback { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Feedback = await _context.Feedbacks
+                .Include(f => f.CompetencyTracker)
+                .Include(f => f.Goal)
+                .Include(f => f.User).ToListAsync();
+        }
+    }
+}
