bdb4d3f0013e4e6c880c1335adaa2d8300e20046
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-10-30 15:30:50 +1030

    Can view feedback given for various students on a dashboard page

--- Areas/Staff/Pages/FeedbackDashboard.cshtml.cs
+++ Areas/Staff/Pages/FeedbackDashboard.cshtml.cs
@@ -1,32 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using StudentPortfolio.Data;
 using StudentPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace StudentPortfolio.Areas.Staff.Pages
 {
-    public class FeedbackDashboard2Model : PageModel
+    [Authorize(Roles = "Staff")]
+    public class FeedbackDashboardModel : PageModel
     {
         private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public FeedbackDashboard2Model(StudentPortfolio.Data.ApplicationDbContext context)
+
+        public FeedbackDashboardModel(StudentPortfolio.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IList<Feedback> Feedback { get;set; } = default!;
+        public List<StudentFeedbackViewModel> StudentFeedback { get; set; } = new List<StudentFeedbackViewModel>();
+
+        public IList<Feedback> GoalFeedback { get;set; } = default!;
+
+        public IList<Feedback> CompFeedback { get; set; } = default!;
+        public ApplicationUser CurrentUser { get; set; }
 
         public async Task OnGetAsync()
         {
-            Feedback = await _context.Feedbacks
-                .Include(f => f.CompetencyTracker)
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            CurrentUser = await _userManager.FindByIdAsync(userId);
+
+            // Gets all the goal feedback
+            var allGoalFeedback = await _context.Feedbacks
+                .Where(f => f.UserId == userId && f.Goal != null)
                 .Include(f => f.Goal)
-                .Include(f => f.User).ToListAsync();
+                    .ThenInclude(g => g.User) 
+                .ToListAsync();
+
+            // Gets all the competency feedback
+            var allCompFeedback = await _context.Feedbacks
+                .Where(f => f.UserId == userId && f.CompetencyTracker != null)
+                .Include(f => f.CompetencyTracker)
+                    .ThenInclude(ct => ct.User) 
+                .Include(f => f.CompetencyTracker)
+                    .ThenInclude(ct => ct.Competency) 
+                .ToListAsync();
+
+            // Checks nulls in case
+            var goalStudents = allGoalFeedback
+                .Where(f => f.Goal?.User != null)
+                .Select(f => f.Goal.User);
+
+            var compStudents = allCompFeedback
+                .Where(f => f.CompetencyTracker?.User != null) 
+                .Select(f => f.CompetencyTracker.User);
+
+            
+            var allStudents = goalStudents.Union(compStudents).Distinct(); // Gets unique list of students
+
+            StudentFeedback = new List<StudentFeedbackViewModel>();
+
+            foreach (var student in allStudents)
+            {
+                var studentModel = new StudentFeedbackViewModel
+                {
+                    Student = student,
+
+                    // Find all feedback for the specific student
+                    GoalFeedback = allGoalFeedback
+                                     .Where(f => f.Goal?.UserId == student.Id)
+                                     .ToList(),
+                    CompFeedback = allCompFeedback
+                                     .Where(f => f.CompetencyTracker?.UserId == student.Id)
+                                     .ToList()
+                };
+                StudentFeedback.Add(studentModel);
+            }
+
         }
     }
+}
+
+public class StudentFeedbackViewModel
+{
+    public ApplicationUser Student { get; set; }
+    public List<Feedback> GoalFeedback { get; set; } = new List<Feedback>();
+    public List<Feedback> CompFeedback { get; set; } = new List<Feedback>();
 }
