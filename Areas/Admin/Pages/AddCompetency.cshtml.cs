d88c5fff2d4752bf94c750628c6a42b917978f93
Otslc <otslc001@mymail.unisa.edu.au>
2025-10-31 13:02:54 +1030

    Add new competency works

---
+++ Areas/Admin/Pages/AddCompetency.cshtml.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentPortfolio.Models;
+
+namespace StudentPortfolio.Areas.Admin.Pages
+{
+    public class AddCompetencyModel : PageModel
+    {
+        private readonly StudentPortfolio.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AddCompetencyModel(StudentPortfolio.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public Competency Competency { get; set; } = new Competency();
+
+        public IList<Competency> ParentCompetencies { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            ParentCompetencies = await _context.Competencies
+                    .Where(i => i.ParentCompetencyId == null)
+                    .ToListAsync();
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Competency.LastUpdated = DateTime.Now;
+
+            _context.Competencies.Add(Competency);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("/Competencies");
+        }
+    }
+}
