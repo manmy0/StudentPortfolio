03c6b01dd867bc48fa24394d1c80ba90e12ae121
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-11-06 15:46:54 +1030

    Updated IdentitySeeder to create default student and staff users as well

--- Models/IdentitySeeder.cs
+++ Models/IdentitySeeder.cs
@@ -26,15 +26,27 @@ namespace StudentPortfolio.Models
             var adminEmail = "admin@gmail.com";
             var adminPassword = "Admin@123";
 
-            // Check if the admin user already exists
-            var userExist = await userManager.FindByEmailAsync(adminEmail);
-            if (userExist == null)
+            // Define student user details
+            var studentEmail = "test@test.com";
+            var studentPassword = "Test1@";
+
+            // Define staff user details
+            var staffEmail = "staff@staff.com";
+            var staffPassword = "Staff1@";
+
+            // Check if the users already exist
+            var adminExist = await userManager.FindByEmailAsync(adminEmail);
+            var studentExist = await userManager.FindByEmailAsync(studentEmail);
+            var staffExist = await userManager.FindByEmailAsync(staffEmail);
+
+            if (adminExist == null)
             {
                 var adminUser = new ApplicationUser
                 {
                     UserName = "admin@gmail.com",
                     Email = adminEmail,
-                    FirstName = "Admin",
+                    FirstName = "Rose",
+                    LastName = "Lalonde",
                     EmailConfirmed = true,
                     CareerDevelopmentPlans = new List<CareerDevelopmentPlan>(),
                     CompetencyTrackers = new List<CompetencyTracker>(),
@@ -48,16 +60,86 @@ namespace StudentPortfolio.Models
                 };
 
                 // Create the admin user
-                var result = await userManager.CreateAsync(adminUser, adminPassword);
-                if (result.Succeeded)
+                var adminResult = await userManager.CreateAsync(adminUser, adminPassword);
+                if (adminResult.Succeeded)
                 {
                     // Assign the Admin role to the user
                     await userManager.AddToRoleAsync(adminUser, "Admin");
                 }
                 else
                 {
-                    throw new Exception("Failed to create the admin user: " + string.Join(", ", result.Errors));
+                    throw new Exception("Failed to create the admin user: " + string.Join(", ", adminResult.Errors));
                 }
+            }
+
+            if (studentExist == null)
+            {
+                var studentUser = new ApplicationUser
+                {
+                    UserName = studentEmail,
+                    Email = studentEmail,
+                    FirstName = "John",
+                    PreferedFirstName = "Johnny",
+                    LastName = "Smith",
+                    EmailConfirmed = true,
+                    CareerDevelopmentPlans = new List<CareerDevelopmentPlan>(),
+                    CompetencyTrackers = new List<CompetencyTracker>(),
+                    ContactsOfInterests = new List<ContactsOfInterest>(),
+                    Goals = new List<Goal>(),
+                    IndustryContactLogs = new List<IndustryContactLog>(),
+                    NetworkingEvents = new List<NetworkingEvent>(),
+                    UserLinks = new List<UserLink>(),
+                    Feedbacks = new List<Feedback>()
+
+                };
+
+                // Create the student user
+                var studentResult = await userManager.CreateAsync(studentUser, studentPassword);
+                if (studentResult.Succeeded)
+                {
+                    // Assign the student role to the user
+                    await userManager.AddToRoleAsync(studentUser, "Student");
+                }
+                else
+                {
+                    throw new Exception("Failed to create the student user: " + string.Join(", ", studentResult.Errors));
+                }
+
+            }
+
+
+            if (staffExist == null)
+            {
+                var staffUser = new ApplicationUser
+                {
+                    UserName = staffEmail,
+                    Email = staffEmail,
+                    FirstName = "Jane",
+                    LastName = "Crocker",
+                    EmailConfirmed = true,
+                    CareerDevelopmentPlans = new List<CareerDevelopmentPlan>(),
+                    CompetencyTrackers = new List<CompetencyTracker>(),
+                    ContactsOfInterests = new List<ContactsOfInterest>(),
+                    Goals = new List<Goal>(),
+                    IndustryContactLogs = new List<IndustryContactLog>(),
+                    NetworkingEvents = new List<NetworkingEvent>(),
+                    UserLinks = new List<UserLink>(),
+                    Feedbacks = new List<Feedback>()
+
+                };
+
+                // Create the staff user
+                var staffResult = await userManager.CreateAsync(staffUser, staffPassword);
+                if (staffResult.Succeeded)
+                {
+                    // Assign the staff role to the user
+                    await userManager.AddToRoleAsync(staffUser, "Staff");
+                }
+                else
+                {
+                    throw new Exception("Failed to create the staff user: " + string.Join(", ", staffResult.Errors));
+                }
+
             }
         }
     }
